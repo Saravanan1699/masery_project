121f94eb9ce5079c6c00838db9c1182e70c031ad
marisct <sctmari21@gmail.com>
2024-07-04 11:22:11 +0530

    bottom bar cart

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'Cart.dart';
 import 'home.dart';
 import 'Categorys_pages/Categories.dart';
 import 'wishlist.dart';
 
-class BottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
   final Function(int) onTap;
   final List<Map<String, dynamic>> favoriteProducts;
 
   BottomBar({required this.onTap, required this.favoriteProducts});
+
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int totalItems = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTotalItems();
+  }
+
+  Future<void> fetchTotalItems() async {
+    try {
+      final response = await http.get(Uri.parse('https://sgitjobs.com/MaseryShoppingNew/public/api/totalitems'));
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        setState(() {
+          totalItems = int.parse(responseData['total_items'] ?? '0');
+        });
+      } else {
+        throw Exception('Failed to load total items');
+      }
+    } catch (e) {
+      print('Error fetching total items: $e');
+      setState(() {
+        totalItems = 0;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +52,45 @@ class BottomBar extends StatelessWidget {
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: 'Wishlist'),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: ''),
+        BottomNavigationBarItem(
+          icon: Stack(
+            children: [
+              Icon(Icons.shopping_cart_outlined),
+              if (totalItems > 0)
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 14,
+                      minHeight: 14,
+                    ),
+                    child: Text(
+                      '$totalItems',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          label: 'Cart',
+        ),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
         BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Profile'),
       ],
       selectedItemColor: Colors.black,
       unselectedItemColor: Colors.black,
       onTap: (index) {
-        onTap(index);
+        widget.onTap(index);
         switch (index) {
           case 0:
             Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
@@ -34,12 +99,12 @@ class BottomBar extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => Wishlist(favoriteProducts: favoriteProducts),
+                builder: (context) => Wishlist(favoriteProducts: widget.favoriteProducts),
               ),
             );
             break;
           case 2:
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Cart(favoriteProducts: favoriteProducts,)));
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Cart(favoriteProducts: widget.favoriteProducts,)));
             break;
           case 3:
             Navigator.push(context, MaterialPageRoute(builder: (context) => Categories()));
